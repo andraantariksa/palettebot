040945724ed8f58bbb4b46ff6fce91ca4efeceb6
Andra Antariksa <andra.antariksa@gmail.com>
2019-05-13 22:56:14 +0700

    Integrate PaletteBot with Facebook

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ extern crate image;
 extern crate pexels;
 extern crate rand;
 extern crate reqwest;
+extern crate serde_json;
 
 const PEXELS_API_KEY: &'static str = "";
 const FACEBOOK_PAGE_ACCESS_TOKEN: &'static str = "";
+
+mod media;
 
 use image::GenericImage as _;
 use image::GenericImageView as _;
@@ -12,24 +15,30 @@ use rand::Rng as _;
 use std::error::Error as _;
 
 fn main() {
-    let p =
-        pexels::Pexels::new(PEXELS_API_KEY.to_string());
-    let searched = p.curated_photo(1, rand::thread_rng().gen::<u32>() % 1000 + 1);
-    let url = searched["photos"][0]["src"]["original"].as_str().unwrap();
-    println!("{}", url);
-    let file_ext = url.split('.').last().unwrap();
+    // Get an image from Pexels
+    let con_pexels = pexels::Pexels::new(PEXELS_API_KEY.to_string());
+    let searched = con_pexels.curated_photo(1, rand::thread_rng().gen::<u32>() % 1000 + 1);
+    let image_url = searched["photos"][0]["src"]["original"].as_str().unwrap();
+    let url = searched["photos"][0]["url"].as_str().unwrap();
+
+    println!("Pexels URL: {}", url);
+    println!("Image URL: {}", image_url);
+
+    let file_ext = image_url.split('.').last().unwrap();
+
     println!("Extension: {}", file_ext);
-    let mut image_file = reqwest::Client::new().get(url).send().unwrap();
+
+    let mut image_file = reqwest::Client::new().get(image_url).send().unwrap();
     let file_path = format!("{}{}", "tmp/input.", file_ext);
     let path = std::path::Path::new(&file_path);
     let display = path.display();
     let mut file = match std::fs::File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Err(why) => panic!("Couldn't create {}: {}", display, why.description()),
         Ok(file) => file,
     };
     match std::io::copy(&mut image_file, &mut file) {
-        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
-        Ok(_) => println!("successfully wrote to {}", display),
+        Err(why) => panic!("Couldn't write to {}: {}", display, why.description()),
+        Ok(_) => println!("Cuccessfully wrote to {}", display),
     }
 
     // Open the image
@@ -38,10 +47,10 @@ fn main() {
     if img.width() > img.height() {
         //img.resize(600, 600, image::Nearest);
         img = img.resize(img.height(), 600, image::Nearest);
-    }else {
+    } else {
         img = img.resize(600, img.width(), image::Nearest);
     }
-    img.save("tmp/output_resized.png").unwrap();
+    //img.save("tmp/output_resized.png").unwrap();
 
     // Preparing the canvas
     let mut canvas = image::ImageBuffer::new(600, 700);
@@ -52,10 +61,13 @@ fn main() {
     img = img.crop(0, 0, 600, 600);
     canvas.copy_from(&img, 0, 0);
 
+    let mut colors_list = Vec::new();
     for i in (0..6 * 2).step_by(2) {
         let color = img.get_pixel(50 * i, rand::thread_rng().gen::<u32>() % 600);
-        let color_hex = format!("#{:X}{:X}{:X}", color.data[0], color.data[1], color.data[2]);
-        println!("{}", color_hex);
+        colors_list.push(format!(
+            "#{:02X}{:02X}{:02X}",
+            color.data[0], color.data[1], color.data[2]
+        ));
         let mut square = image::ImageBuffer::new(50, 50);
         for (_, _, pixel) in square.enumerate_pixels_mut() {
             *pixel = color;
@@ -65,4 +77,15 @@ fn main() {
 
     // Save the output
     canvas.save("tmp/output.png").unwrap();
+
+    let caption = format!(
+        "Colors extracted: {}\n Image from: {}",
+        colors_list.join(", "),
+        url
+    );
+
+    // Connect to Facebook
+    let con_fb = media::facebook::Facebook::new(FACEBOOK_PAGE_ACCESS_TOKEN);
+    let con_fb_image_post_id = con_fb.post_image("tmp/output.png", String::new());
+    con_fb.comment(&con_fb_image_post_id, &caption)
 }
--- src/media/facebook.rs
+++ src/media/facebook.rs
@@ -1,4 +1,39 @@
-fn post_image() {
-	let result = reqwest::Client::new().get(url).send().unwrap();
-	println!("{:?}", result);
+pub struct Facebook {
+    access_token: String,
+    reqwest_client: reqwest::Client,
+}
+
+impl Facebook {
+    pub fn new(access_token: &str) -> Facebook {
+        Facebook {
+            access_token: access_token.to_string(),
+            reqwest_client: reqwest::Client::new(),
+        }
+    }
+
+    pub fn post_image(&self, img_path: &str, message: String) -> String {
+        let form = reqwest::multipart::Form::new()
+            .text("access_token", self.access_token.clone())
+            .text("message", message)
+            .file("source", img_path)
+            .unwrap();
+        let mut req = self
+            .reqwest_client
+            .post("https://graph.facebook.com/me/photos")
+            .multipart(form)
+            .send()
+            .unwrap();
+        serde_json::from_str::<serde_json::Value>(&req.text().unwrap()).unwrap()["post_id"]
+            .as_str()
+            .unwrap()
+            .to_string()
+    }
+
+    pub fn comment(&self, id: &str, message: &str) {
+        self.reqwest_client
+            .post(&format!("https://graph.facebook.com/{}/comments", id))
+            .query(&[("message", message), ("access_token", &self.access_token)])
+            .send()
+            .unwrap();
+    }
 }
